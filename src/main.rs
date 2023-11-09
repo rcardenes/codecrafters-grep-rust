76a3fe582f8bd84e2f06607eed7d8bc9d49dd824
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-11-09 05:21:09 -0300

    Add support for alphanumeric pattern

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,7 @@
-use anyhow::{bail, Result};
 use std::env;
 use std::io;
 use std::process;
 use grep_starter_rust::regex::RegexPattern;
-
-fn parse_pattern(pattern: &str) -> Result<RegexPattern> {
-    let mut stream = pattern.chars();
-    let res = match stream.next() {
-        Some('\\') => {
-            match stream.next() {
-                Some('d') => Ok(RegexPattern::Digit),
-                Some(chr) => Ok(RegexPattern::Char(chr)),
-                None => bail!("trailing backlash (\\)"),
-            }
-        }
-        Some(chr) => {
-            Ok(RegexPattern::Char(chr))
-        }
-        None => {
-            Ok(RegexPattern::Empty)
-        }
-    };
-
-    if stream.next().is_none() {
-        res
-    } else {
-        bail!("Unhandled pattern: {pattern}")
-    }
-}
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
@@ -40,7 +14,7 @@ fn main() {
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
-    match parse_pattern(&pattern) {
+    match RegexPattern::parse(&pattern) {
         Ok(pat) => {
             if pat.is_contained_in(&input_line) {
                 process::exit(0)
--- src/regex.rs
+++ src/regex.rs
@@ -1,22 +1,62 @@
+use anyhow::{bail, Result};
+
 pub enum RegexPattern {
     Char(char),
+    AlphaNum,
     Digit,
     Empty,
 }
 
 impl RegexPattern {
+    pub fn parse(pattern: &str) -> Result<Self> {
+        let mut stream = pattern.chars();
+        let res = match stream.next() {
+            Some('\\') => {
+                match stream.next() {
+                    Some('d') => Ok(RegexPattern::Digit),
+                    Some('w') => Ok(RegexPattern::AlphaNum),
+                    Some(chr) => Ok(RegexPattern::Char(chr)),
+                    None => bail!("trailing backlash (\\)"),
+                }
+            }
+            Some(chr) => {
+                Ok(RegexPattern::Char(chr))
+            }
+            None => {
+                Ok(RegexPattern::Empty)
+            }
+        };
+
+        if stream.next().is_none() {
+            res
+        } else {
+            bail!("Unhandled pattern: {pattern}")
+        }
+    }
+
     pub fn is_contained_in(&self, haystack: &str) -> bool {
         match self {
             RegexPattern::Char(pat) => {
                 haystack.contains(*pat)
             }
             RegexPattern::Digit => {
-                let digits = '0'..='9';
                 let mut chars = haystack.chars();
 
                 while let Some(chr) = chars.next() {
-                    if digits.contains(&chr) {
-                        return true
+                    match chr {
+                        '0'..='9' => return true,
+                        _ => {}
+                    }
+                }
+                false
+            }
+            RegexPattern::AlphaNum => {
+                let mut chars = haystack.chars();
+
+                while let Some(chr) = chars.next() {
+                    match chr {
+                        '0'..='9' | 'a'..='z' | 'A'..='Z' | '_' => return true,
+                        _ => {}
                     }
                 }
                 false
