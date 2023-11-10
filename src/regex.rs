a4e94542ca99e0cdae2c34222fbd2179bc83733a
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-11-09 21:25:30 -0300

    Support start of string/line anchor

--- src/regex.rs
+++ src/regex.rs
@@ -1,26 +1,92 @@
 use anyhow::{bail, Result};
 
-#[derive(Debug)]
-pub enum RegexPattern {
+pub enum RegexClass {
     Char(char),
     AlphaNum,
     Digit,
     CharGroup((Vec<char>, bool)),
-    Sequence(Vec<RegexPattern>),
+    Sequence(Vec<RegexClass>),
+}
+
+impl RegexClass {
+    fn min_size(&self) -> usize {
+        match self {
+            RegexClass::Sequence(seq) => {
+                seq.iter().fold(0, |acc, item| { acc + item.min_size() } )
+            }
+            _ => 1,
+        }
+    }
+
+    fn matches(&self, haystack: &str) -> (bool, usize) {
+        let mut it = haystack.chars();
+        match self {
+            RegexClass::Char(pat) => {
+                (it.next().is_some_and(|c| c == *pat), 1)
+            }
+            RegexClass::Digit => {
+                let mt = it.next().is_some_and(|c| match c {
+                    '0'..='9' => true,
+                    _ => false
+                });
+                (mt, 1)
+            }
+            RegexClass::AlphaNum => {
+                let mt = it.next().is_some_and(|c| match c {
+                    '0'..='9' | 'a'..='z' | 'A'..='Z' | '_' => true,
+                    _ => false
+                });
+                (mt, 1)
+            }
+            RegexClass::CharGroup((set, polarity)) => {
+                let mt = it.next().is_some_and(|c| if set.contains(&c) {
+                    *polarity
+                } else {
+                    !*polarity
+                });
+                (mt, 1)
+            }
+            RegexClass::Sequence(seq) => {
+                let mut consumed = 0usize;
+
+                for pat in seq {
+                    let (matches, length) = pat.matches(&haystack[consumed..]);
+                    if !matches {
+                        return (false, 0)
+                    } else {
+                        consumed += length;
+                    }
+                }
+
+                (true, consumed)
+            }
+        }
+    }
+}
+
+pub struct RegexPattern {
+    at_start: bool,
+    sequence: RegexClass,
 }
 
 impl RegexPattern {
     pub fn parse(pattern: &str) -> Result<Self> {
+        let mut at_start = false;
         let mut seq = vec![];
         let mut stream = pattern.chars();
+
+        if pattern.starts_with('^') {
+            stream.next();
+            at_start = true;
+        }
 
         while let Some(chr) = stream.next() {
             match chr {
                 '\\' => {
                     match stream.next() {
-                        Some('d') => seq.push(RegexPattern::Digit),
-                        Some('w') => seq.push(RegexPattern::AlphaNum),
-                        Some(chr) => seq.push(RegexPattern::Char(chr)),
+                        Some('d') => seq.push(RegexClass::Digit),
+                        Some('w') => seq.push(RegexClass::AlphaNum),
+                        Some(chr) => seq.push(RegexClass::Char(chr)),
                         None => bail!("trailing backlash (\\)"),
                     }
                 }
@@ -32,7 +98,7 @@ impl RegexPattern {
                     while let Some(chr) = stream.next() {
                         match chr {
                             ']' => {
-                                seq.push(RegexPattern::CharGroup((set, polarity)));
+                                seq.push(RegexClass::CharGroup((set, polarity)));
                                 closed = true;
                                 break;
                             },
@@ -46,74 +112,31 @@ impl RegexPattern {
                     }
                 }
                 _ => {
-                    seq.push(RegexPattern::Char(chr))
+                    seq.push(RegexClass::Char(chr))
                 }
             }
         };
 
-        Ok(RegexPattern::Sequence(seq))
-    }
-
-    fn min_size(&self) -> usize {
-        match self {
-            RegexPattern::Sequence(seq) => {
-                seq.iter().fold(0, |acc, item| { acc + item.min_size() } )
-            }
-            _ => 1,
-        }
-    }
-
-    fn matches(&self, haystack: &str) -> (bool, usize) {
-        let mut it = haystack.chars();
-        let result = match self {
-            RegexPattern::Char(pat) => {
-                it.next().is_some_and(|c| c == *pat)
-            }
-            RegexPattern::Digit => {
-                it.next().is_some_and(|c| match c {
-                    '0'..='9' => true,
-                    _ => false
-                })
-            }
-            RegexPattern::AlphaNum => {
-                it.next().is_some_and(|c| match c {
-                    '0'..='9' | 'a'..='z' | 'A'..='Z' | '_' => true,
-                    _ => false
-                })
-            }
-            RegexPattern::CharGroup((set, polarity)) => {
-                it.next().is_some_and(|c| if set.contains(&c) {
-                    *polarity
-                } else {
-                    !*polarity
-                })
-            }
-            RegexPattern::Sequence(..) => todo!(),
-        };
-
-        (result, 1)
+        Ok(RegexPattern {
+            at_start,
+            sequence: RegexClass::Sequence(seq)
+        })
     }
 
     pub fn is_contained_in(&self, haystack: &str) -> Result<bool> {
-        if self.min_size() > haystack.len() {
+        let min_size = self.sequence.min_size();
+        if min_size > haystack.len() {
             return Ok(false)
         }
-        match self {
-            RegexPattern::Sequence(seq) => {
-                for offset in 0..=(haystack.len() - self.min_size()) {
-                    let mut valid = true;
-                    let mut consumed = 0;
 
-                    for pat in seq {
-                        let (matches, length) = pat.matches(&haystack[(offset + consumed)..]);
-                        if !matches {
-                            valid = false;
-                            break;
-                        } else {
-                            consumed += length;
-                        }
-                    }
-                    if valid {
+        match &self.sequence {
+            RegexClass::Sequence(..) => {
+                if self.at_start {
+                    return Ok(self.sequence.matches(haystack).0)
+                }
+
+                for offset in 0..=(haystack.len() - min_size) {
+                    if self.sequence.matches(&haystack[(offset)..]).0 {
                         return Ok(true)
                     }
                 }
